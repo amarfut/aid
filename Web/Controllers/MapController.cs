02fd4bb5b42aeeaad04dcf7a424e5240516382e5
Oleksandr Marfut <omarf@softserveinc.com>
2019-12-24 21:56:23 +0100

    wip

---
+++ Web/Controllers/MapController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class MapController : BaseController
+    {
+        [HttpGet("map/{id}/", Name = "Map")]
+        public async Task<IActionResult> Map(string id)
+        {
+            if (id == "dotnetweb")
+            {
+                return View("DotNetWeb");
+            }
+
+
+            return View();
+        }
+    }
+}
